94814279adb6d7122264660743e7ed0a548a2d45
aster <137767097+aster-void@users.noreply.github.com>
2025-08-22 03:36:20 +0900

    agenix: fix secret references

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,17 +1,10 @@
-# agenix CLI設定 - 受信者（SSH公開鍵）を列挙
-# このファイルはNixOS構成にimportしない（CLI専用）
 let
-  # ユーザーの公開鍵（~/.ssh/id_ed25519.pub または https://github.com/aster-void.keys から取得）
-  user_aster = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGQGwjkGWrw..."; # TODO: 実際の公開鍵に置き換える
-  
-  # ホストの公開鍵（ssh-keyscan carbon または /etc/ssh/ssh_host_ed25519_key.pub から取得）
-  host_carbon = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHmGjkGWrw..."; # TODO: 実際のホスト公開鍵に置き換える
-in
-{
+  publicKeys = builtins.fromJSON (builtins.readFile ../config/ssh-authorized-keys.json);
+in {
   # 暗号化ファイルと対応する受信者を定義
   # 例: パスワードファイル
-  "password.age".publicKeys = [ user_aster host_carbon ];
-  
+  "password.age".publicKeys = publicKeys;
+
   # 他のシークレットファイルもここに追加
   # "database-password.age".publicKeys = [ user_aster host_carbon ];
   # "api-key.age".publicKeys = [ user_aster host_carbon ];
