747a585aa2f4fa2610a78a506760cb58c1e80a65
aster <137767097+aster-void@users.noreply.github.com>
2025-09-10 03:14:47 +0900

    nix-minecraft tests

--- hosts/carbon/services/minecraft.nix
+++ hosts/carbon/services/minecraft.nix
@@ -20,22 +20,42 @@
     eula = true;
     openFirewall = true;
 
-    servers = {
-      hardcore = {
-        enable = true;
-        package = pkgs.paperServers.paper;
-        serverProperties = {
-          server-port = 25566;
-          gamemode = 0;
-          hardcore = true;
+    servers =
+      {
+        hardcore = {
+          enable = true;
+          package = pkgs.paperServers.paper;
+          serverProperties = {
+            server-port = 25566;
+            gamemode = 0;
+            hardcore = true;
+          };
+          enableReload = true;
+          extraReload = ''
+            minecraftctl send hardcore reload
+            minecraftctl send hardcore whitelist reload
+          '';
         };
-        enableReload = true;
-        extraReload = ''
-          minecraftctl send hardcore reload
-          minecraftctl send hardcore whitelist reload
-        '';
-      };
-    };
+      }
+      // (
+        let
+          mkServer = package: port: manager: {
+            enable = true;
+            serverProperties = {
+              server-port = port;
+              gamemode = 1;
+              difficulty = 0;
+            };
+            managementSystem.tmux.enable = manager == "tmux";
+            managementSystem.systemd-socket.enable = manager == "systemd-socket";
+          };
+        in {
+          van = mkServer pkgs.vanillaServers.vanilla 25567 "tmux";
+          fab = mkServer pkgs.fabricServers.fabric 25568 "systemd-socket";
+          qui = mkServer pkgs.quiltServers.quilt 25569 "tmux";
+          vel = mkServer pkgs.velocityServers.velocity 25570 "systemd-socket";
+        }
+      );
   };
 
   services.minecraft = {
