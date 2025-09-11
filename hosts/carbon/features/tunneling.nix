c92e696bd79c7dffd52bec9c20bea08f920f37e6
aster <137767097+aster-void@users.noreply.github.com>
2025-09-12 01:09:13 +0900

    setup cf tunneling properly

--- hosts/carbon/features/tunneling.nix
+++ hosts/carbon/features/tunneling.nix
@@ -1,12 +1,12 @@
 {config, ...}: {
   # Cloudflare tunnel configuration
-  age.secrets.cloudflared-tunnel-token.file = ../../../secrets/cloudflared-tunnel-token.age;
+  age.secrets.cloudflared-carbon.file = ../../../secrets/cloudflared/carbon.json.age;
 
   services.cloudflared = {
     enable = true;
     tunnels = {
-      "dde4e79f-c550-42fa-820e-d53bdae1e9eb" = {
-        credentialsFile = config.age.secrets.cloudflared-tunnel-token.path;
+      "carbon" = {
+        credentialsFile = config.age.secrets.cloudflared-carbon.path;
         default = "http_status:404";
         ingress = {
           "carbon.aster-void.dev" = "ssh://localhost:22";
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,5 +2,5 @@ let
   publicKeys = builtins.fromJSON (builtins.readFile ../config/ssh-authorized-keys.json);
 in {
   "secrets/playit.toml.age".publicKeys = publicKeys;
-  "secrets/cloudflared-tunnel-token.age".publicKeys = publicKeys;
+  "secrets/cloudflared/carbon.json.age".publicKeys = publicKeys;
 }
