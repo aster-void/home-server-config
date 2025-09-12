e8ef224fcce1efc3389f9489273a4df92050798b
aster <137767097+aster-void@users.noreply.github.com>
2025-09-13 00:21:38 +0900

    cloudflared: add secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,4 +3,6 @@ let
 in {
   "secrets/playit.toml.age".publicKeys = publicKeys;
   "secrets/cloudflared/carbon.json.age".publicKeys = publicKeys;
+  "secrets/cloudflared/cert.pem.age".publicKeys = publicKeys;
+  "secrets/cloudflared/certificate.pem.age".publicKeys = publicKeys;
 }
