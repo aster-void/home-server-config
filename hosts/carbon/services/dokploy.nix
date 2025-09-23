87678faa5995d3c7bf5eb4ad9693c649d0662e8b
aster <137767097+aster-void@users.noreply.github.com>
2025-09-24 00:15:41 +0900

    dokploy: explicitly disable rootless docker

--- hosts/carbon/services/dokploy.nix
+++ hosts/carbon/services/dokploy.nix
@@ -24,6 +24,7 @@
       imports = [inputs.nix-dokploy.nixosModules.default];
       virtualisation.docker.enable = true;
       virtualisation.docker.daemon.settings.live-restore = false;
+      virtualization.docker.rootless.enable = false;
       services.dokploy.enable = true;
       system.stateVersion = "25.11";
     };
