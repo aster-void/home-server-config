04ca9b289e48a4510b367aa7ca75b9e7abffd274
aster <137767097+aster-void@users.noreply.github.com>
2025-10-14 13:35:05 +0900

    networking: enable cloudflare-warp

--- common/features/networking.nix
+++ common/features/networking.nix
@@ -3,19 +3,20 @@
   pkgs,
   ...
 }: {
-  # Cloudflare WARP VPN setup
-  environment.systemPackages = with pkgs; [
-    cloudflared
-    cloudflare-cli
-    cloudflare-warp
-  ];
-
   # Basic networking setup
   networking.networkmanager.enable = true;
   networking.hostName = meta.hostname;
 
   # SSH access
   networking.firewall.allowedTCPPorts = [22];
+
+  # Cloudflare WARP VPN setup
+  services.cloudflare-warp.enable = true;
+  environment.systemPackages = with pkgs; [
+    cloudflared
+    cloudflare-cli
+    cloudflare-warp
+  ];
 
   # mDNS discovery for local network
   services.avahi = {
