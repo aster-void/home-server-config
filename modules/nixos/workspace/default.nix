311bda0f7b41ea510b047ebf14b52c7e36833196
aster <137767097+aster-void@users.noreply.github.com>
2025-11-09 00:06:56 +0900

    Refactor workspace module layout

--- modules/nixos/workspace/default.nix
+++ modules/nixos/workspace/default.nix
@@ -1,6 +1,19 @@
-{
-  imports = [
-    ./container.nix
-    ./firewall.nix
+{config, ...}: {
+  containers.workspace = {
+    autoStart = true;
+    privateNetwork = false;
+    config = {
+      _module.args = {
+        sshAuthorizedKeys = config.meta.sshAuthorizedKeys;
+      };
+      imports = [
+        ./container/default.nix
+      ];
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = [
+    2222
+    2223
   ];
 }
