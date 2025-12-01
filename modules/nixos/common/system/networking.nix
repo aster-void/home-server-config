32d2bc162bd3527e54e6a5d539057fa842e82168
aster <137767097+aster-void@users.noreply.github.com>
2025-12-01 21:39:40 +0900

    Revert "avahi: exclude Docker interfaces from mDNS"
    This reverts commit e1897f676310b6d70c4680165ff20f5bf53140f2.

--- modules/nixos/common/system/networking.nix
+++ modules/nixos/common/system/networking.nix
@@ -23,8 +23,6 @@
     enable = true;
     nssmdns4 = true;
     openFirewall = true;
-    # Exclude Docker and virtual interfaces to prevent interference
-    denyInterfaces = ["veth*" "docker*"];
     publish = {
       enable = true;
       addresses = true;
